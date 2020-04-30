f888bfb7011411e100ca0ffa36ffd4094f7fcdde
MORGAN James <james.morgan10@student.education.wa.edu.au>
2020-04-30 10:54:27 +0800

    working stuff

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,67 @@ namespace DinnerPlanner
 		public MainWindow()
 		{
 			InitializeComponent();
-		}
+            MenuPlan.ItemsSource = Meal.GetMeals();
+        }
 	}
 }
+public class Meal : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    private string food;
+
+    public string Food
+    {
+        get { return food; }
+        set
+        {
+            food = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public static ObservableCollection<Meal> GetMeals()
+    {
+        var meals = new ObservableCollection<Meal>();
+
+        meals.Add(new Meal()
+        {
+            Food = "Ali"
+        });
+
+        meals.Add(new Meal()
+        {
+            Food = "Ahmed",
+        });
+
+        meals.Add(new Meal()
+        {
+            Food = "Amjad",
+        });
+
+        meals.Add(new Meal()
+        {
+            Food = "Waqas",
+        });
+
+        meals.Add(new Meal()
+        {
+            Food = "Bilal",
+        });
+
+        meals.Add(new Meal()
+        {
+            Food = "Waqar",
+        });
+
+        return meals;
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+
+
+

0d315c691f0103c0f9d1983b5d0c0368ae668143
MORGAN James <james.morgan10@student.education.wa.edu.au>
2020-05-05 13:29:36 +0800

    fixed all major issues and added saving

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,19 +2,12 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace DinnerPlanner
 {
@@ -28,104 +21,142 @@ namespace DinnerPlanner
 			InitializeComponent();
 			MenuPlan.ItemsSource = Meal.GetMeals();
 		}
-	}
-}
-public class Meal : INotifyPropertyChanged
-{
-	public event PropertyChangedEventHandler PropertyChanged;
-	private string mon;
-	private string tues;
-	private string wed;
-	private string thurs;
-	private string fri;
-	private string sat;
-	private string sun;
 
-	public string Mon
-	{
-		get { return mon; }
-		set
+		private void button_Click(object sender, RoutedEventArgs e)
 		{
-			mon = value;
-			OnPropertyChanged();
+			SaveDataToXml();
 		}
-	}
-	public string Tues
-	{
-		get { return tues; }
-		set
-		{
-			tues = value;
-			OnPropertyChanged();
-		}
-	}
-	public string Wed
-	{
-		get { return wed; }
-		set
-		{
-			wed = value;
-			OnPropertyChanged();
-		}
-	}
-	public string Thurs
-	{
-		get { return thurs; }
-		set
-		{
-			thurs = value;
-			OnPropertyChanged();
-		}
-	}
-	public string Fri
-	{
-		get { return fri; }
-		set
-		{
-			fri = value;
-			OnPropertyChanged();
-		}
-	}
-	public string Sat
-	{
-		get { return sat; }
-		set
-		{
-			sat = value;
-			OnPropertyChanged();
-		}
-	}
-	public string Sun
-	{
-		get { return sun; }
-		set
-		{
-			sun = value;
-			OnPropertyChanged();
-		}
-	}
 
-	public static ObservableCollection<Meal> GetMeals()
-	{
-		var meals = new ObservableCollection<Meal>();
-
-		meals.Add(new Meal()
+		private void SaveDataToXml()
 		{
-			Mon = "Ali",
-			Tues = "test",
-			Wed = "burger",
-			Thurs = "chips",
-			Fri = "dog",
-			Sat = "cat",
-			Sun = "gjkjf"
-		});
 
-		return meals;
+			var meals = MenuPlan.Items;
+			Console.WriteLine(meals);
+			List<DinnerPlanner.Meal> NewMeals = meals.OfType<DinnerPlanner.Meal>().ToList();
+			var oc = new ObservableCollection<Meal>(NewMeals);
+			//meals.Add(new Meal()
+			//{
+			//	Mon = "Ali",
+			//Tues = "test",
+			//Wed = "burger",
+			//Thurs = "chips",
+			//Fri = "dog",
+			//Sat = "cat",
+			//Sun = "gjkjf"
+			//});
+
+			XmlSerializer xs = new XmlSerializer(typeof(ObservableCollection<Meal>));
+			using (StreamWriter wr = new StreamWriter("Meals.xml"))
+			{
+				xs.Serialize(wr, oc);
+			}
+		}
+
+
+
+
+
+
 	}
-
-	protected void OnPropertyChanged([CallerMemberName] string name = null)
+	public class Meal : INotifyPropertyChanged
 	{
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		public event PropertyChangedEventHandler PropertyChanged;
+		private string mon;
+		private string tues;
+		private string wed;
+		private string thurs;
+		private string fri;
+		private string sat;
+		private string sun;
+
+		public string Mon
+		{
+			get { return mon; }
+			set
+			{
+				mon = value;
+				OnPropertyChanged();
+			}
+		}
+		public string Tues
+		{
+			get { return tues; }
+			set
+			{
+				tues = value;
+				OnPropertyChanged();
+			}
+		}
+		public string Wed
+		{
+			get { return wed; }
+			set
+			{
+				wed = value;
+				OnPropertyChanged();
+			}
+		}
+		public string Thurs
+		{
+			get { return thurs; }
+			set
+			{
+				thurs = value;
+				OnPropertyChanged();
+			}
+		}
+		public string Fri
+		{
+			get { return fri; }
+			set
+			{
+				fri = value;
+				OnPropertyChanged();
+			}
+		}
+		public string Sat
+		{
+			get { return sat; }
+			set
+			{
+				sat = value;
+				OnPropertyChanged();
+			}
+		}
+		public string Sun
+		{
+			get { return sun; }
+			set
+			{
+				sun = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public static ObservableCollection<Meal> GetMeals()
+		{
+			var meals = new ObservableCollection<Meal>();
+
+			////https://stackoverflow.com/questions/1194931/what-is-the-easiest-way-to-save-an-
+			XmlSerializer xs2 = new XmlSerializer(typeof(ObservableCollection<Meal>));
+			using (StreamReader rd = new StreamReader("Meals.xml"))
+			{
+				meals = xs2.Deserialize(rd) as ObservableCollection<Meal>;
+			}
+
+			return meals;
+
+
+
+
+
+			//https://stackoverflow.com/questions/8863393/datagrid-loading-saving-xml
+		}
+
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
 	}
 }
 

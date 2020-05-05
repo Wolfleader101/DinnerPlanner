cdd0e9ac1163ea4cc901083addda85038207cd0c
Wolfleader <james.morgan@iinet.net.au>
2020-05-05 15:27:59 +0800

    revert

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -29,6 +31,32 @@ namespace DinnerPlanner
 		{
 			InitializeComponent();
 			MenuPlan.ItemsSource = Meal.GetMeals();
+		}
+
+		private void SaveMeals_Click(object sender, RoutedEventArgs e)
+		{
+
+			//need this to update from JSON --- use it for generate meals
+			//MenuPlan.ItemsSource = Meal.GetMeals();
+
+			var row = MenuPlan.Items[0];
+			Console.WriteLine(row);
+			// Update JSON File
+			UpdateJson(row);
+
+			
+			
+
+		}
+
+		public void UpdateJson(object meals)
+		{
+			// serialize JSON directly to a file
+			using (StreamWriter file = File.CreateText(@"./test.json"))
+			{
+				JsonSerializer serializer = new JsonSerializer();
+				serializer.Serialize(file, meals);
+			}
 		}
 	}
 }
@@ -109,35 +137,51 @@ public class Meal : INotifyPropertyChanged
 
 	public static ObservableCollection<Meal> GetMeals()
 	{
-		var meals = new ObservableCollection<Meal>();
 
-		meals.Add(new Meal()
+		// read JSON directly from a file
+		using (StreamReader file = File.OpenText(@"./test.json"))
+		using (JsonTextReader reader = new JsonTextReader(file))
 		{
-			Mon = "Ali",
-			Tues = "test",
-			Wed = "burger",
-			Thurs = "chips",
-			Fri = "dog",
-			Sat = "cat",
-			Sun = "gjkjf"
-		});
+			JObject output = (JObject)JToken.ReadFrom(reader);
+			string o = output.ToObject<string>();
+			var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(o);
+			foreach (var kv in dict)
+			{
+				Console.WriteLine(kv.Key + ":" + kv.Value);
+			}
 
-		return meals;
+			return dict;
+		}
+
+		
+
+		// create new meals object
+		//var meals = new ObservableCollection<Meal>();
+		//meals.Add(new Meal()
+		//{
+		//	Mon = "Ali",
+		//	Tues = "test",
+		//	Wed = "burger",
+		//	Thurs = "chips",
+		//	Fri = "dog",
+		//	Sat = "cat",
+		//	Sun = "gjkjf"
+		//});
 	}
-	protected void OnPropertyChanged([CallerMemberName] string name = null)
+
+	private static void MakeJson(ObservableCollection<Meal> meals)
 	{
-	PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-
-		Console.WriteLine(this);
-		// serialize JSON to a string and then write string to a file
-		File.WriteAllText(@"./test.json", JsonConvert.SerializeObject(name));
-
-		//open file stream
+		// serialize JSON directly to a file
 		using (StreamWriter file = File.CreateText(@"./test.json"))
 		{
 			JsonSerializer serializer = new JsonSerializer();
-			serializer.Serialize(file, name);
+			serializer.Serialize(file, meals);
 		}
+	}
+
+	protected void OnPropertyChanged([CallerMemberName] string name = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 	}
 }
 
